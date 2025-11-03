c292a475ebca444c6ad864a80a4228866b3bf734
Nadman Alvee <nadmanalvee@gmail.com>
2025-11-03 21:28:26 +0600

    day_3

---
+++ team.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+  int n, vote[3], sol_count = 0;
+  cin >> n;
+  while (n--){
+    int one_count = 0;
+    for (int i = 0; i < 3; i++){
+      cin >> vote[i];
+      if (vote[i] == 1){
+        one_count++;
+      }
+    }
+    if (one_count > 1){
+      sol_count++;
+    }
+  }
+  cout << sol_count << endl;
+}
