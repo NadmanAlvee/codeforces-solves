f7a223cade84f0daeb3bd4734222a43b6bfe4f85
Nadman Alvee <nadmanalvee@gmail.com>
2025-11-03 07:35:42 +0600

    day_2

---
+++ Way-Too-Long-Words.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <string>
+using namespace std;
+int main()
+{
+    int n;
+    string word;
+    cin >> n;
+    while(n--){
+      cin >> word;
+      word.length() > 10 ? cout << word[0] << word.length()-2 << word[word.length()-1] << endl : cout << word << endl;
+    }
+}
