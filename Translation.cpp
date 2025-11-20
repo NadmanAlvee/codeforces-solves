1e1847d02a8a8e9fbae94d41864f2eae97df9069
Nadman Alvee <nadmanalvee@gmail.com>
2025-11-20 07:03:38 +0600

    day_20

---
+++ Translation.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+int main() {
+  string a, b;
+  cin >> a >> b;
+  for(int i = 0, j = b.length()-1; i<a.length(); i++, j--){
+    if(a[i] != b[j]){
+      cout << "NO";
+      return 0;
+    }
+  }
+  cout << "YES";
+}
