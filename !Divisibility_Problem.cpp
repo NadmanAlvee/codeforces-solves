7378d3665ca5a4a3d31de84cfaab54d079644066
Nadman Alvee <nadmanalvee@gmail.com>
2025-12-04 22:40:15 +0600

    day_34

---
+++ !Divisibility_Problem.cpp
@@ -0,0 +1,24 @@
+// Solution 1: Exceeds time limit
+
+/*
+#include <iostream>
+using namespace std;
+int main(){
+  long int n, count, a, b;
+  cin >> n;
+  while(n--){
+    count = 0;
+    cin >> a >> b;
+    while(a % b != 0){
+      count++;
+      ++a;
+    }
+    cout << count << endl;
+  }
+}
+*/
+
+// Solution 2: Youtube
+
+
+
