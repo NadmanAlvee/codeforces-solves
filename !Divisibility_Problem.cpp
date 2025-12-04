8e6cd5628f7c3c68400f74825532861bed37554f
Nadman Alvee <nadmanalvee@gmail.com>
2025-12-04 22:58:40 +0600

    day_34

--- !Divisibility_Problem.cpp
+++ !Divisibility_Problem.cpp
@@ -20,5 +20,17 @@ int main(){
 
 // Solution 2: Youtube
 
-
-
+#include <iostream>
+using namespace std;
+int main(){
+  int n, a, b;
+  cin >> n;
+  while(n--){
+    cin >> a >> b;
+    if(a % b == 0){
+      cout << 0 << endl;
+    } else {
+      cout << b - (a%b) << endl;
+    }
+  }
+}
